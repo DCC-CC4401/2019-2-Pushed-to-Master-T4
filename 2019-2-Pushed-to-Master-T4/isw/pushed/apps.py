2831daf5440c56b6f582da0872bbd2019f7c4b6f
Diego Sandoval <daslwebmaster@gmail.com>
2019-10-02 18:46:43 -0300

    Revert "Integrando Django"
    This reverts commit dc807f37ee29503d4281dd62556910d1c5e0e0a2.

--- 2019-2-Pushed-to-Master-T4/isw/pushed/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PushedConfig(AppConfig):
-    name = 'pushed'
--- 2019-2-Pushed-to-Master-T4/isw/pushed/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('perfil', views.profile , name='profile'),
-    path('login', views.login , name='login'),
-]
