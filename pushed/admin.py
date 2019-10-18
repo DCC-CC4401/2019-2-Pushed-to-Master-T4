a0701dd4ddf7fa4f573c4e3a2cac9457ee548b23
Diego Sandoval <daslwebmaster@gmail.com>
2019-10-18 17:50:08 -0300

    Usuarios corregidos

--- pushed/admin.py
+++ pushed/admin.py
@@ -3,10 +3,4 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 from .forms import RegisterForm
 
-class CustomUserAdmin(UserAdmin):
-    add_form = RegisterForm
-    model = User
-    ordering = ()
-
-
-admin.site.register(User,CustomUserAdmin)
+admin.site.register(User)
